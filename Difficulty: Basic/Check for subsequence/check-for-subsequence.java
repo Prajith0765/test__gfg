41ff9bbd2a18ace5e6a4a455bca88864f8b195e7
Prajith P <129357819+Prajith0765@users.noreply.github.com>
2025-09-28 12:22:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for subsequence/check-for-subsequence.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    boolean isSubSequence(String A, String B) {
+        
+        int  i = 0, j = 0;
+        
+        while(i < A.length() && j < B.length()){
+            
+            if(A.charAt(i) == B.charAt(j)){
+                i++;
+            }
+            
+            j++;
+        }
+        
+        return i == A.length();
+    }
+}
